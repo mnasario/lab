f14a5fc174f52a96d5b94de70f96908820818aa6
Marcos Nasario <marcos.nasario@akelius.de>
2020-04-27 17:36:50 +0200

    Creating new conflict

---
+++ creating_conflicts.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+creatingNewConflict()
+{
+    git checkout $1
+    git pull
+    sleep 1
+    echo "New line " >> README.md
+    git commit -am "Creating new conflict"
+    git push
+}
+
+echo "Adding a new conflict in DEV"
+creatingNewConflict dev
+
+echo "Adding a new conflict in TEST CONFLICT branch"
+creatingNewConflict test-with-conflict
