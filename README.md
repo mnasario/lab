92fe00244dc2589850c620455ee3de03b973edd8
Marcos Nasario <marcos.nasario@akelius.de>
2020-04-28 18:20:28 +0200

    Creating new conflict

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Project to test frameworks
 # Testing spring-data-sqlfile framework
 
 
+Tue Apr 28 18:20:28 CEST 2020 -> dev 
+
--- creating_conflicts.sh
+++ creating_conflicts.sh
@@ -4,13 +4,15 @@ creatingNewConflict()
 {
     git checkout $1
     git pull
-    echo "[$(date)] - New value \n" >> README.md
+    echo "$(date) -> $1 \n" >> README.md
     git commit -am "Creating new conflict"
     git push
 }
 
 echo "Adding a new conflict in DEV"
 creatingNewConflict dev
+
+sleep 1
 
 echo "Adding a new conflict in TEST CONFLICT branch"
 creatingNewConflict test-with-conflict
