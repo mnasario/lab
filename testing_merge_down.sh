e841572f35b32a89f71a413c2f7fbce6709b7fb9
Marcos Nasario <marcos.nasario@akelius.de>
2020-04-28 17:05:16 +0200

    Creating new conflict

---
+++ testing_merge_down.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+git checkout dev
+echo "Adding a new line"
+git pull
+echo "New line to $1 \n" >> README.md
+git commit -am "Creating new conflict"
+git push
+
+git checkout test
+git checkout -b merge-branch
+git merge --ff-only dev
+git commit -am "Testing merge down"
+
+git checkout test
+git merge --no-ff merge-branch
+git push
